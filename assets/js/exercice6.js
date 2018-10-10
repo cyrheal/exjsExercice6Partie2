14edc14690f43bb4bcd0d931457af1336815ac79
simonot <cyrilsimonot@yahoo.fr>
2018-10-10 21:05:56 +0200

    exercice6partie2

---
+++ assets/js/exercice6.js
@@ -0,0 +1,6 @@
+function myFunc(image){
+  document.getElementById(image).src='assets/images/'+image+'_2.jpg';
+}
+function returnimg(image){
+  document.getElementById(image).src='assets/images/'+image+'.jpg';
+}
